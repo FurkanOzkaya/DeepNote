41a06b6e8f827ae1e49ea06f9729a3251a03ce02
melezteori <sinan_371@hotmail.com>
2019-11-02 23:34:09 +0300

    sinan
    others profile page has been changed.

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/design/dialog/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/design/dialog/R.java
@@ -962,8 +962,8 @@ public final class R {
         public static final int notification_template_icon_low_bg = 0x7f0800ac;
         public static final int notification_tile_bg = 0x7f0800ad;
         public static final int notify_panel_notification_icon_bg = 0x7f0800ae;
-        public static final int tooltip_frame_dark = 0x7f0800c9;
-        public static final int tooltip_frame_light = 0x7f0800ca;
+        public static final int tooltip_frame_dark = 0x7f0800cb;
+        public static final int tooltip_frame_light = 0x7f0800cc;
     }
     public static final class id {
         private id() {}
